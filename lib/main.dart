1a1fa1c0d76391108585eb62cda2c3a63bd348cd
Abraham Peter <theclassical001@gmail.com>
2021-11-22 17:57:43 +0100

    container and text features are explained

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,30 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("App In All"),
       ),
-      body: Container(
-        child: Text("hello there"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all (8),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center ,
+          decoration: BoxDecoration(
+            color: Colors. red,
+            shape: BoxShape.circle,
+            gradient: LinearGradient(colors: [
+              Colors.purple,
+              Colors.orange,
+            ]),
+            ),
+          child: Text("I am a Box", 
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize:20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+        ),
       )
     );
   }
